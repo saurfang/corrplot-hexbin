4c3ba180017af3774d1e1e8449eb28b6de703001
Saurfang <forest.fang@outlook.com>
2014-08-09 15:21:11 -0400

    better column header render
    improve calculation, use angled header instead

--- app/scripts/controllers/correlation.js
+++ app/scripts/controllers/correlation.js
@@ -10,24 +10,51 @@
 angular.module('heatmapApp')
     .controller('correlationCtrl', ['$rootScope', '$scope', 'dataFactory', 'ngTableParams', '$window',
         function ($rootScope, $scope, dataFactory, ngTableParams, $window) {
-            $scope.style = {rowHead: {}, row: {}, colHead: {}, table: {}};
+            $scope.style = {
+                rotate: false,
+                row: {},
+                rowHead: {'text-align': 'right'},
+                colHead: {},
+                table: {},
+                rotateDiv: {},
+                rotateSpan: {}
+            };
             var showVal = true;
-            var adjustStyle = function() {
-                var maxWidth = d3.max($scope.vars.map(function(v){return v.width();})) + 16;
-                $scope.style.colHead.height =  maxWidth + 'px';
+            var adjustStyle = function () {
+                var maxWidth = d3.max($scope.vars.map(function (v) {
+                    return v.width();
+                }));
                 $scope.style.rowHead.width = maxWidth + 'px';
 
                 var side = ($('#correlation').parent().width() - maxWidth) / $scope.vars.length;
-                $scope.style.colHead.width =  side + 'px';
+                $scope.style.colHead.width = side + 'px';
                 $scope.style.row.height = side + 'px';
 
                 showVal = side > 32;
+                var fontSize = side < 12 ? side : 12, headerFontSize = fontSize,
+                    rotate = maxWidth > side;
+                if (rotate && fontSize === side) {
+                    headerFontSize *= Math.sin(Math.PI / 4);
+                }
+                $scope.style.table['font-size'] = fontSize + 'px';
+                $scope.style.rotateSpan['font-size'] = headerFontSize + 'px';
 
-                //FIXME: This is not ideal because font-size and side have circular dependency
-                $scope.style.table['font-size'] = (side < 14 ? side : 14) + 'px';
+                if (rotate) {
+                    var adjustedWidth = maxWidth * headerFontSize / 12,
+                        rotatedHeight = adjustedWidth * Math.sin(Math.PI / 4) ,
+                        paddedHeight = rotatedHeight + headerFontSize * Math.cos(Math.PI / 4) * 2;
+                    $scope.style.colHead.height = paddedHeight + 'px';
+                    $scope.style.rotateDiv.left = paddedHeight / 2;
+                    $scope.style.rotateSpan.left = (headerFontSize - adjustedWidth) / 2;
+                    $scope.style.rotateSpan.bottom = rotatedHeight / 2;
+                    $scope.style.rotateSpan.width = adjustedWidth;
+                } else {
+                    $scope.style.colHead.height = headerFontSize;
+                }
+                $scope.style.rotate = rotate;
             };
 
-            angular.element($window).bind('resize', function() {
+            angular.element($window).bind('resize', function () {
                 adjustStyle();
                 return $scope.$apply();
             });
@@ -35,18 +62,33 @@ angular.module('heatmapApp')
             $scope.vars = [];
             var values = [];
             $scope.correlations = [];
-            var parseCSV = function(response) {
+            var parseCSV = function (response) {
                 values = d3.csv.parse(response);
                 $scope.vars = Object.keys(values[0]);
 
                 //Compute correlations
                 var correlations = [];
-                $scope.vars.forEach(function(varX){
+                $scope.vars.forEach(function (varX, idX) {
                     var correlation = {name: varX, values: {}};
-                    var x = values.map(function(d){ return +d[varX]; });
-                    $scope.vars.forEach(function(varY){
-                        var y = values.map(function(d){ return +d[varY]; });
-                        correlation.values[varY] = dataFactory.getPearsonsCorrelation(x, y);
+                    var x = function (d) {
+                        return +d[varX];
+                    };
+                    $scope.vars.forEach(function (varY, idY) {
+                        var corr = NaN;
+                        if (idX === idY) {
+                            if(values.map(x).filter(isFinite).length){
+                                corr = 1;
+                            }
+                        } else if (idX < idY) {
+                            var y = function (d) {
+                                return +d[varY];
+                            };
+                            corr = dataFactory.getPearsonsCorrelation(values, x, y);
+                        } else {
+                            corr = correlations[idY].values[varX];
+                        }
+
+                        correlation.values[varY] = corr;
                     });
                     correlations.push(correlation);
                 });
@@ -56,33 +98,35 @@ angular.module('heatmapApp')
                 adjustStyle();
             };
             $scope.dataset = 'iris.csv';
-            $scope.$watch('dataset', function(dataset){
+            $scope.$watch('dataset', function (dataset) {
                 dataFactory.fetch(dataset).then(parseCSV);
             });
 
             var reader = new FileReader();
-            reader.onload = function(e) {
+            reader.onload = function (e) {
                 parseCSV(reader.result);
             }
 
-            $scope.uploadFile = function(files) {
+            $scope.uploadFile = function (files) {
                 reader.readAsText(files[0]);
             };
 
-            $scope.updateSelection = function(rowX, varY){
-                if(isFinite(rowX.values[varY])){
+            $scope.updateSelection = function (rowX, varY) {
+                if (isFinite(rowX.values[varY])) {
                     var varX = rowX.name;
-                    if(varX === varY){
+                    if (varX === varY) {
                         $rootScope.selectedVar = varX;
-                        $rootScope.selectedValues = values.map(function(d){
+                        $rootScope.selectedValues = values.map(function (d) {
                             return [+d[varX], 1];
                         })
                         $rootScope.$emit('histogramChanged');
-                    }else{
+                    } else {
                         $rootScope.selectedVars = [varX, varY];
-                        $rootScope.selectedHexbins = values.map(function(d){
+                        $rootScope.selectedHexbins = values.map(function (d) {
                             return [+d[varX], +d[varY], 1];
-                        })
+                        }).filter(function (d) {
+                            return isFinite(d[0]) && isFinite(d[1]);
+                        });
                         $rootScope.$emit('hexbinChanged');
                     }
                 }
@@ -90,32 +134,32 @@ angular.module('heatmapApp')
 
             $scope.format = d3.format('%.2f');
             $scope.isFinite = isFinite;
-            $scope.tooltip = function(row, name) {
-                if(row.name === name){
+            $scope.tooltip = function (row, name) {
+                if (row.name === name) {
                     return name;
-                }else{
+                } else {
                     var tooltip = row.name + ' vs. ' + name;
                     var value = row.values[name];
-                    if(!showVal && isFinite(value)){
+                    if (!showVal && isFinite(value)) {
                         tooltip += '<br/>' + $scope.format(value);
                     }
                     return tooltip;
                 }
             };
-            $scope.showVal = function(row, name){
+            $scope.showVal = function (row, name) {
                 return showVal && row.name !== name && isFinite(row.values[name]);
             };
 
             $scope.colorScale = d3.scale.linear()
-                    .domain(d3.range(-1, 1, .2))
-                    .range(colorbrewer.RdYlGn[11])
-                    .interpolate(d3.interpolateLab);
+                .domain(d3.range(-1, 1, .2))
+                .range(colorbrewer.RdYlGn[11])
+                .interpolate(d3.interpolateLab);
 
-            $scope.color = function(row, name){
-                if(isNaN(row.values[name])){
-                   return 'white';
-                }else{
-                   return $scope.colorScale(row.values[name]);
+            $scope.color = function (row, name) {
+                if (isNaN(row.values[name])) {
+                    return 'white';
+                } else {
+                    return $scope.colorScale(row.values[name]);
                 }
             };
 
